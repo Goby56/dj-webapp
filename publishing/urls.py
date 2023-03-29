b5fa252709beef2be05eceda101f346522068f66
Karl Rosengren <60710855+Goby56@users.noreply.github.com>
2023-03-29 11:01:37 +0200

    post styling kinda

--- publishing/urls.py
+++ publishing/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "publishing"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    # path("/submit", views.submit, name="submit")
 ]
